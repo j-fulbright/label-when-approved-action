82405906b940d35c6f4637dcee7fb80b57fdbd1c
J. Fulbright <jeremiah@iclasspro.com>
2023-07-21 14:36:58 -0500

    add missing function (#2)

--- dist/index.js
+++ dist/index.js
@@ -9795,6 +9795,15 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
+function getRequiredEnv(key) {
+  const value = process.env[key];
+  if (value === undefined) {
+    const message = `${key} was not defined.`;
+    throw new Error(message);
+  }
+  return value;
+}
+
 function verboseOutput(name, value) {
   core.info(`Setting output: ${name}: ${value}`);
   core.setOutput(name, value);
